6a14fc3c1123b4ee71322c6c7955c94cd311632b
#Chris <chris.frindt@uebermaps.com>
2016-08-18 22:42:03 +0200

    ErsterCommit Chris - falls nicht lüuft nach pull, mach Build - rebuild

---
+++ app/src/main/java/com/searchandfound/snf/Utils/APIBuilder.java
@@ -0,0 +1,75 @@
+package com.searchandfound.snf.Utils;
+
+import com.searchandfound.snf.Interfaces.SearchandFoundService;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
+/**
+ * Created by #Chris on 18.08.2016.
+ */
+public class APIBuilder {
+
+    /* public used paramenters */
+    public static int COUNT = 10;
+
+    /* private section */
+    private static APIBuilder instance = null;
+    private static SearchandFoundService service = null;
+
+    private static int MODE = APIBuilder.PROD;
+    private static final int PROD = 3;
+
+
+
+    private static final String PRODUCTION_SCHEMA = "https";
+    private static final String PRODUCTION_HOST = "HIER KOMMT SERVER ADRESSE";
+
+
+
+
+    protected APIBuilder() {
+        /* Exists only to defeat instantiation */
+    }
+
+    public static APIBuilder getInstance() {
+        if (instance == null) {
+            instance = new APIBuilder();
+        }
+        return instance;
+    }
+
+    public SearchandFoundService getService() {
+        if (service == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(APIBuilder.getBase())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            /*// Accepting unsafe(no CA) Connection to dev.geocha.com
+            if(MODE == API.DEV){
+
+                retrofit.client().setHostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                });
+
+            }*/
+            service = retrofit.create(SearchandFoundService.class);
+        }
+        return service;
+    }
+
+
+    public static String getBase() {
+        switch (APIBuilder.MODE){
+            case (APIBuilder.PROD): return PRODUCTION_SCHEMA+ "://" + PRODUCTION_HOST;
+            default: return PRODUCTION_SCHEMA+ "://" + PRODUCTION_HOST;
+        }
+    }
+
+
+
+}
+
